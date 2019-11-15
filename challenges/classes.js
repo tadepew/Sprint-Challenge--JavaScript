5f0b297739f4f9f951b66c3d269a3481b1e2011a
Tristan Depew <tristan.depew@gmail.com>
2019-11-15 11:36:29 -0500

    finished prototypes and classes

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,4 +1,29 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
+class CuboidMaker {
+    constructor(attrs) {
+    this.length = attrs.length,
+    this.width = attrs.width,
+    this.height = attrs.height
+  }
+  volume() {
+    return this.length * this.width * this.height;
+  }
+  surfaceArea() {
+    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+  }
+  }
+
+  
+  /* == Step 4: Create a new object that uses CuboidMaker ==
+    Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
+    Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
+  */
+  const cuboid = new CuboidMaker ({
+    length: 4,
+    width: 5,
+    height: 5,
+  })
+
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
